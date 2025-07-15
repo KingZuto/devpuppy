e974f5665e6d1008a1a7445072f66f4b44d1bcab
조성욱 <>
2025-07-15 15:48:03 +0900

    Fix CodeBuild IAM permissions and buildspec.yml syntax

--- terraform/modules/cicd/main.tf
+++ terraform/modules/cicd/main.tf
@@ -53,6 +53,18 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       {
         Effect = "Allow"
         Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.codepipeline_artifacts.arn,
+          "${aws_s3_bucket.codepipeline_artifacts.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
           "cloudfront:CreateInvalidation"
         ]
         Resource = "*"
